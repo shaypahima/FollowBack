00b4a3e2df140a8f8c92f894a15306130ac5a79b
shayp <shay.p@comtecglobal.com>
2026-01-04 14:52:04 +0200

    Enhance backend configuration and dependencies
    - Updated .gitignore to exclude generated Prisma files.
    - Added dotenv package for environment variable management.
    - Modified prisma.config.ts to use process.env for database URL.
    - Created .env.example file for environment variable setup.
    - Refactored index.ts to include logging and environment configuration for server setup.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,27 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import profiles from './routes'
+import { logger } from 'hono/logger'
+import { prettyJSON } from 'hono/pretty-json'
+import 'dotenv/config'
+
+
+const PORT = process.env.PORT || 3000
+const ENV = process.env.NODE_ENV || 'development'
+const HOST = process.env.HOST || '0.0.0.0'
+
+console.log(`Server is running on ${HOST}:${PORT} in ${ENV} mode`)
 
 const app = new Hono()
 app.use(cors())
+app.use(logger())
 
 app.route('/profiles',profiles)
 
-export default app
+export default {
+  host: HOST,
+  port: PORT,
+  prettyJSON: prettyJSON(),
+  logger: logger(),
+  fetch: app.fetch,
+}
